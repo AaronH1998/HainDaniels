c475a98883ea6e43d6d3f1da48c5af8605f4aa04
AaronHodgson <aaronhodgson1998@gmail.com>
2020-10-05 16:49:40 +0100

    formatted navbar and added logo

--- HainDanielsApi/HainDanielsApi/Models/Product.cs
+++ HainDanielsApi/HainDanielsApi/Models/Product.cs
@@ -5,17 +5,25 @@ namespace HainDanielsApi.Models
     public class Product
     {
         [Key]
+        [Required]
         public int M3Item { get; set; }
 
+        [Required]
         public string Description { get; set; }
+
         public string JarCode { get; set; }
         public string VarietyCode { get; set; }
         public string Zambelli { get; set; }
         public int Label { get; set; }
         public string Cap { get; set; }
         public string Domino { get; set; }
+
+        [Required]
         public int UnitsPerCase { get; set; }
+
+        [Required]
         public double NetWeight { get; set; }
+
         public int PalletQyt { get; set; }
         public int SalesDays { get; set; }
         public int BatchOrderMultiples { get; set; }
